e1c7fe729020635a81eb70623b5425afcdb808c7
Nicole <nverauesfa.ineval@gmail.com>
2025-07-30 20:13:58 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hola soy Nicolin 
+          holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         </a>
       </header>
     </div>
